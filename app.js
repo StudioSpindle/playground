d0f9254665276f158d3d396d30540529ce8f0136
Vledder <remi@studiospindle.nl>
2018-08-14 10:48:39 +0200

    create dynamic routing for express

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
- 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
+const appDir = path.dirname(require.main.filename);
+
+require('./controller/routes.js')(app, path, appDir);
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/css', express.static(path.join(__dirname, 'css')));
-
-/**
- * JSDoc pages
- * TODO: Refactor to a more scalable setup, where generated pages are automatically available
- */
-
-app.get('/out/index.html', function (req, res) {
-    res.sendFile(path.join(__dirname + '/out/index.html'));
-});
-
-app.get('/out/global.html', function (req, res) {
-    res.sendFile(path.join(__dirname + '/out/global.html'));
-});
-
-app.get('/out/index.html', function (req, res) {
-    res.sendFile(path.join(__dirname + '/out/index.js.html'));
-});
-
-app.get('/out/index.js.html', function (req, res) {
-    res.sendFile(path.join(__dirname + '/out/index.js.html'));
-});
-
 app.use('/out/fonts', express.static(path.join(__dirname, '/out/fonts')));
 app.use('/out/scripts', express.static(path.join(__dirname, '/out/scripts')));
 app.use('/out/styles', express.static(path.join(__dirname, '/out/styles')));
