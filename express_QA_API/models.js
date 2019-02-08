bcdabe3469a30135c9c5caec94840fd5d6e81097
Vledder <remi@studiospindle.nl>
2019-02-08 12:31:16 +0100

    add missing methods for voting and updating answers

--- express_QA_API/models.js
+++ express_QA_API/models.js
@@ -25,6 +25,20 @@ var AnswerSchema = new Schema({
   votes: {  type: Number, default: 0 }
 });
 
+AnswerSchema.method("update", function(updates, callback) {
+  Object.assign(this, updates, {updatedAt: new Date()});
+  this.parent().save(callback);
+});
+
+AnswerSchema.method("vote", function(vote, callback) {
+  if(vote === "up") {
+    this.votes += 1;
+  } else {
+    this.votes -= 1;
+  }
+  this.parent().save(callback);
+});
+
 var QuestionSchema = new Schema({
   text: String,
   createdAt: { type: Date, default: Date.now },
